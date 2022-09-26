bd786f8697cf3d659df7160f3ac342d27f8f87ed
Ru1ka <wotb5ks@gmail.com>
2022-09-26 16:50:03 +0300

    26.09 result

--- static/script.js
+++ static/script.js
@@ -1,8 +1,41 @@
 function discord_copy() {
-    var copyText = document.getElementById("hide_txt");
-    copyText.select();
-    document.execCommand("copy");
-    
-    var tooltip = document.getElementById("tooltip");
-    tooltip.innerHTML = "Copied: " + copyText.value;
+  var copyText = document.getElementById("hide_txt");
+  copyText.select();
+  document.execCommand("copy");
+
+  var tooltip = document.getElementById("tooltip");
+  tooltip.innerHTML = "Copied: " + copyText.value;
+}
+
+const Stages = [
+  document.getElementById("0"),
+  document.getElementById("1"),
+  document.getElementById("2"),
+  document.getElementById("3"),
+];
+
+var cur = 0;
+
+function left_click() {
+  console.log("left click");
+  if (cur - 1 >= 0) {
+    Stages[cur].classList.add("hide");
+    Stages[cur].classList.remove("appearance");
+    cur--
+    Stages[cur].classList.add("appearance");
+    Stages[cur].classList.remove("hide");
+    Stages[cur].classList.remove("disappearance");
   }
+}
+
+function right_click() {
+  console.log("right click");
+  if (cur + 1 <= 3) {
+    Stages[cur].classList.add("hide");
+    Stages[cur].classList.remove("appearance");
+    cur++
+    Stages[cur].classList.add("appearance");
+    Stages[cur].classList.remove("hide");
+  }
+
+}
